7ded819874eb95833074923029f8c71bd57c9c24
mtownsend509 <mgtownsend509@gmail.com>
2023-05-14 09:22:28 -0700

    tuning

--- src/components/button.js
+++ src/components/button.js
@@ -7,15 +7,15 @@ function Button () {
     let fired = false
     const [score, setScore] = useState(0)
 
-    let firstNote = ((window.innerWidth/2) + 448)
-    let secondNote = ((window.innerWidth/2) + 336)
-    let thirdNote = ((window.innerWidth/2) + 224)
-    let fourthNote = ((window.innerWidth/2) + 112)
+    let firstNote = ((window.innerWidth/2) + 440)
+    let secondNote = ((window.innerWidth/2) + 330)
+    let thirdNote = ((window.innerWidth/2) + 220)
+    let fourthNote = ((window.innerWidth/2) + 110)
     let fifthNote = (window.innerWidth/2)
-    let sixthNote = ((window.innerWidth/2) - 112)
-    let seventhNote = ((window.innerWidth/2) - 224)
-    let eighthNote = ((window.innerWidth/2) - 336)
-    let ninthNote = ((window.innerWidth/2) - 448)
+    let sixthNote = ((window.innerWidth/2) - 110)
+    let seventhNote = ((window.innerWidth/2) - 220)
+    let eighthNote = ((window.innerWidth/2) - 330)
+    let ninthNote = ((window.innerWidth/2) - 440)
 
 
     const [measure, setMeasure] = useState(1);
